84c2bc4e0e1c28c7e9f622df45eb38959c11e49d
ericoshet <maister.bk.ru>
2025-10-15 17:58:46 +0300

    Server Start

---
+++ server/src/app.js
@@ -0,0 +1,12 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// serverConfig(app);
+// app.use('./api', apiRouter);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
