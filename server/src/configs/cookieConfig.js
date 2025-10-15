909e423b3e24fc63b004cc023d6e775a76241132
ericoshet <maister.bk.ru>
2025-10-15 21:33:41 +0300

    Validated Tokens & Cookies, Auorization Service, controller & Router Complite

---
+++ server/src/configs/cookieConfig.js
@@ -0,0 +1,14 @@
+const jwtConfig = require("./jwtConfig");
+
+const cookieConfig = {
+  access: {
+    maxAge: jwtConfig.access.expiresIn,   // время жизни куки
+    httpOnly: true,                       // статус
+  },
+  refresh: {
+    maxAge: jwtConfig.refresh.expiresIn,  // время жизни куки
+    httpOnly: true,                       // статус
+  },
+};
+
+module.exports = cookieConfig;
