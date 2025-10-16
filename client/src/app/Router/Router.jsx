3a141fd1b879d73a37bb4684a4195db1867e93a7
ericoshet <maister.bk.ru>
2025-10-16 12:09:48 +0300

    Add AdminStatus in App.jsx, add SignUp SignIn & Profile with props in Router.jsx, make up SignIn & SignUp pages, add LogOut method in NavBar

--- client/src/app/Router/Router.jsx
+++ client/src/app/Router/Router.jsx
@@ -1,5 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Layout from "../Layout/Layout";
+import SignInPage from "../../pages/AuthPages/SignInPage";
+import SignUpPage from "../../pages/AuthPages/SignUpPage"
+
+// TODO Создать и импортировать сюда страницу Админа "ProfilePage"
+// Логика logout прописана в NavBar
 
 export default function Router() {
   return (
@@ -7,10 +12,10 @@ export default function Router() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<></>} />
-          <Route path="/signup" element={<></>} />
-          <Route path="/signin" element={<></>} />
-          <Route path="/signout" element={<></>} />
-          <Route path="/profile" element={<></>} />
+          <Route path="/signup" element={<SignUpPage setUser={setUser}/>} />
+          <Route path="/signin" element={<SignInPage setUser={setUser}/>} />
+          {/* <Route path="/signout" element={<></>} /> */}  
+          <Route path="/profile" element={<>ProfilePage user={user}</>} />  
           <Route path="*" element={<h1>Нет контента</h1>} />
         </Route>
       </Routes>
